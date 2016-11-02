c37c1bb9963186aba39a2c5092a9334582511096
Luca Koval <lucakoval@me.com>
2016-11-02 18:52:02 -0400

    got rid of old code
    yee

--- gridvisualizer.py
+++ gridvisualizer.py
@@ -1,21 +1,3 @@
-"""import numpy as np
-import matplotlib.pyplot as plt
-from random import randint
-
-# Make some random data to represent your r, g, b bands.
-# ny, nx = 2, 3
-# r, g, b = [np.random.random(ny*nx).reshape((ny, nx)) for _ in range(3)]
-# r = [randint(0,72) for _ in range(3)];
-ny, nx = 3, 3
-r = [0 for _ in range(3)];
-g = [randint(0,255).reshape((ny, nx)) for _ in range (3)];
-b = [0 for _ in range (3)];
-
-c = np.dstack((r,g,b))
-
-plt.imshow(c, interpolation='nearest')
-plt.show()"""
-
 import matplotlib as mpl
 import matplotlib.pyplot as pyplot
 import numpy as np
