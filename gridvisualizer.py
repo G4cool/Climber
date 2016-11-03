cb1406850069f3165af8aad1911470f8fa464639
Luca Koval <lucakoval@me.com>
2016-11-02 22:19:22 -0400

    Kinda works
    yee

--- gridvisualizer.py
+++ gridvisualizer.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 from random import randint
 import math
 
-xMax, yMax = 100, 100
+xMax, yMax = 150, 150
 heightMax = 50
 numHills = 10
-descentDistance = 5
+descentDistance = 15
 
 x = np.arange(xMax)
 y = np.arange(yMax)
@@ -17,12 +17,12 @@ for _ in range (numHills):
 	xRand, yRand = randint(0,(xMax - 1)), randint(0,(yMax - 1))
 	for i in range (descentDistance):
 		for j in range (descentDistance):
-			if (10 - descentDistance/2 - i + xRand <= xMax) & (10 - descentDistance/2 - j + yRand <= yMax):
+			if (descentDistance/2 - i + xRand <= xMax) & (descentDistance/2 - j + yRand <= yMax):
 				dist = math.sqrt(((descentDistance/2 - i)*(descentDistance/2 - i)) + ((descentDistance/2 - j)*(descentDistance/2 - j)))
 				if dist == 0:
-					z[(descentDistance/2 - i + xRand),(descentDistance/2 - j + yRand)] = heightMax
+					z[(descentDistance/2 - i + xRand - 1),(descentDistance/2 - j + yRand - 1)] = heightMax
 				else:
-					z[(descentDistance/2 - i + xRand),(descentDistance/2 - j + yRand)] = 20 * (descentDistance/2)/dist
+					z[(descentDistance/2 - i + xRand - 1),(descentDistance/2 - j + yRand - 1)] = 20 * (descentDistance/2)/dist
 
 plt.pcolormesh(x,y,z)
 plt.colorbar()
