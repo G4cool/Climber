143069c5642a6d626bf724b10ebaa0c0bd0e4591
Luca Koval <lucakoval@me.com>
2016-11-02 22:12:35 -0400

    Cleaned it up
    yee

--- gridvisualizer.py
+++ gridvisualizer.py
@@ -1,32 +1,3 @@
-"""
-import matplotlib as mpl
-import matplotlib.pyplot as pyplot
-import numpy as np
-from random import randint
-
-x1, x2, x3, x4, x5, y1, y2, y3, y4, y5 = randint(0,500), randint(0,500), randint(0,500), randint(0,500), randint(0,500), randint(0,500), randint(0,500), randint(0,500), randint(0,500), randint(0,500)
-
-# make values from -5 to 5, for this example
-zvals = np.random.rand(500,500)*10-5
-
-# make a color map of fixed colors
-cmap = mpl.colors.ListedColormap(['black','green','white'])
-bounds=[-6,-2,2,6]
-norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
-
-fig = pyplot.figure(2)
-
-cmap2 = mpl.colors.LinearSegmentedColormap.from_list('my_colormap',
-                                           ['black','green','white'],
-                                           256)
-
-img2 = pyplot.imshow(zvals,interpolation='nearest',
-                    cmap = cmap2,
-                    origin='lower')
-
-pyplot.colorbar(img2,cmap=cmap2)
-
-pyplot.show()"""
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
