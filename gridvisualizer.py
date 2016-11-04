a8e15cf0394a749beee9ae49270210b2f8779389
Luca Koval <lucakoval@me.com>
2016-11-03 20:50:52 -0400

    Layering kinda works, have to keeping adding for multiple layers
    yee

--- gridvisualizer.py
+++ gridvisualizer.py
@@ -8,21 +8,33 @@ xMax, yMax = 150, 150
 heightMax = 50
 numHills = 10
 descentDistance = 15
+descentDistanceMultiplier = 3
 
 x = np.arange(xMax)
 y = np.arange(yMax)
 z = np.zeros([xMax,yMax])
+zVals = np.zeros([xMax,yMax])
 
 for _ in range (numHills):
 	xRand, yRand = randint(0,(xMax - 1)), randint(0,(yMax - 1))
-	for i in range (descentDistance):
-		for j in range (descentDistance):
-			if (descentDistance/2 - i + xRand <= xMax) & (descentDistance/2 - j + yRand <= yMax):
-				dist = math.sqrt(((descentDistance/2 - i)*(descentDistance/2 - i)) + ((descentDistance/2 - j)*(descentDistance/2 - j)))
+	for i in range (descentDistance * descentDistanceMultiplier):
+		for j in range (descentDistance * descentDistanceMultiplier):
+			if (descentDistance*descentDistanceMultiplier/2 - i + xRand <= xMax) & (descentDistance*descentDistanceMultiplier/2 - j + yRand <= yMax):
+				dist = math.sqrt(((descentDistance*descentDistanceMultiplier/2 - i)*(descentDistance*descentDistanceMultiplier/2 - i)) + ((descentDistance*descentDistanceMultiplier/2 - j)*(descentDistance*descentDistanceMultiplier/2 - j)))
 				if dist == 0:
-					z[(descentDistance/2 - i + xRand - 1),(descentDistance/2 - j + yRand - 1)] = heightMax
+					if zVals[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] == 0:
+						z[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = heightMax
+						zVals[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = heightMax
+					else:
+						z[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = heightMax + zVals[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)]
 				else:
-					z[(descentDistance/2 - i + xRand - 1),(descentDistance/2 - j + yRand - 1)] = 20 * (descentDistance/2)/dist
+					if zVals[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] == 0:
+						z[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = 20 * (descentDistance*descentDistanceMultiplier/2)/dist
+						zVals[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = 20 * (descentDistance*descentDistanceMultiplier/2)/dist
+						# z[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = 1000
+					else:
+						z[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = 20 * (descentDistance*descentDistanceMultiplier/2)/dist + zVals[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)]
+						# z[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] = 1000
 
 plt.pcolormesh(x,y,z)
 plt.colorbar()
