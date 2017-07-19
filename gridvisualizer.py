6daaa50556dbc89679bd8801f624ec0338085519
Luca Koval <lucakoval@me.com>
2017-07-19 12:06:21 -0400

    Got Perlin Noise Working
    yee

--- gridvisualizer.py
+++ gridvisualizer.py
@@ -1,12 +1,15 @@
 import spiral
+import perlinnoise
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from random import randint
 import math
-from scipy.ndimage.filters import gaussian_filter
+from random import randint
+# import pyglet
+# from scipy.ndimage.filters import gaussian_filter
 
-xMax, yMax = 150, 150
+xMax, yMax = 100, 100
 heightMax = 50
 numHills = 10
 descentDistance = 150
@@ -23,11 +26,33 @@ zVals = np.zeros([xMax,yMax])
 iterations = 300
 raiseList = spiral.asciiSpiral(iterations)
 
+lin = np.linspace(0,5,100,endpoint=False)
+x,y = np.meshgrid(lin,lin) # FIX3: I thought I had to invert x and y here but it was a mistake
+
+w, h = 100, 100
+perlinArray = [[0 for i in range(w)] for j in range(h)]
+
+seed = randint(1, 100)
+
+perlinArray = perlinnoise.perlin2(x,y,seed=seed)
+# print perlinnoise.perlin2(x,y,seed=2)
+
+for i in range(0, 100):
+	for j in range(0, 100):
+		z[i][j] += perlinArray[i][j]
+
+"""
+for i in range(-1*xMax, xMax):
+	for j in range(-1*yMax, yMax):
+		z[i][j] += perlinnoise.perlin(i, j)"""
+
+"""
 for i in range(-1*xMax, xMax):
 	for j in range(-1*yMax, yMax):
 		for k in range(iterations):
 			if raiseList[k][0] == i and raiseList[k][1] == j:
 				z[i][j] += heightMax
+"""
 
 """
 for _ in range (numHills):
@@ -44,10 +69,19 @@ for _ in range (numHills):
 					zVals[(descentDistance*descentDistanceMultiplier/2 - i + xRand - 1),(descentDistance*descentDistanceMultiplier/2 - j + yRand - 1)] += 20 * (descentDistance*descentDistanceMultiplier/2)/dist
 """
 
-blurredZ = gaussian_filter(z, sigma=5)
+# blurredZ = gaussian_filter(z, sigma=5)
 
-plt.pcolormesh(x,y,blurredZ, cmap='Greens_r')
+plt.pcolormesh(x,y,z, cmap='Greens_r')
 plt.colorbar()
+
+"""
+win = pyglet.window.Window()
+ 
+@win.event
+def on_draw():
+        win.clear()
+ 
+pyglet.app.run()"""
 
 plt.show()
 
--- perlinnoise.py
+++ perlinnoise.py
@@ -22,6 +22,7 @@ def dotGridGradient(ix, iy, x, y):
      return (dx*gradients[iy][ix] + dy*gradients[iy][ix])
 
 def perlin(x, y):
+     # print 'in perlin()'
      # x0 = (x > 0.0 ? x : x - 1)
      while x > 0:
           x = x - 1
@@ -46,19 +47,66 @@ def perlin(x, y):
      value = lerp(ix0, ix1, sy)
 
      return value
+"""
+x = np.arange(10)
+y = np.arange(10)
+z = np.zeros([10,10])
 
-x = np.arange(100)
-y = np.arange(100)
-z = np.zeros([100,100])
-
-for i in range (100):
-     for j in range (100):
+for i in range (10):
+     for j in range (10):
           z[i,j] = perlin(i,j)
 
 plt.pcolormesh(x,y,z)
 plt.colorbar()
 
-plt.show()
+plt.show()"""
+
+def perlin2(x,y,seed=0):
+    # permutation table
+    np.random.seed(seed)
+    p = np.arange(256,dtype=int)
+    np.random.shuffle(p)
+    #p = np.stack([p,p]).flatten()
+    p = np.stack([p,p]).flatten()
+    # coordinates of the top-left
+    xi = x.astype(int)
+    yi = y.astype(int)
+    # internal coordinates
+    xf = x - xi
+    yf = y - yi
+    # fade factors
+    u = fade(xf)
+    v = fade(yf)
+    # noise components
+    n00 = gradient(p[p[xi]+yi],xf,yf)
+    n01 = gradient(p[p[xi]+yi+1],xf,yf-1)
+    n11 = gradient(p[p[xi+1]+yi+1],xf-1,yf-1)
+    n10 = gradient(p[p[xi+1]+yi],xf-1,yf)
+    # combine noises
+    x1 = lerp2(n00,n10,u)
+    x2 = lerp2(n01,n11,u) # FIX1: I was using n10 instead of n01
+    # print lerp2(x1,x2,v)
+    # print len(lerp2(x1,x2,v))
+    return lerp2(x1,x2,v) # FIX2: I also had to reverse x1 and x2 here
+
+def lerp2(a,b,x):
+    "linear interpolation"
+    return a + x * (b-a)
+
+def fade(t):
+    "6t^5 - 15t^4 + 10t^3"
+    return 6 * t**5 - 15 * t**4 + 10 * t**3
+
+def gradient(h,x,y):
+    "grad converts h to the right gradient vector and return the dot product with (x,y)"
+    vectors = np.array([[0,1],[0,-1],[1,0],[-1,0]])
+    g = vectors[h%4]
+    return g[:,:,0] * x + g[:,:,1] * y
+
+lin = np.linspace(0,5,100,endpoint=False)
+x,y = np.meshgrid(lin,lin) # FIX3: I thought I had to invert x and y here but it was a mistake
+
+# plt.imshow(perlin2(x,y,seed=2),origin='upper')
 
 
 
